4cb6e4032e6820ad87e63a1b35dbccac7343d9f0
CarolineP2023 <127094876+CarolineP2023@users.noreply.github.com>
2023-06-27 07:30:07 -0400

    Flask tools first assignment

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, render_template, redirect, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "surveys"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.route('/')
+def home_page():
+    """start survey"""
+
+    return render_template('home.html', survey=survey)
+
+
+
+@app.route('/start')
+def start_survey():
+    """used to start the survey redirects to the first question"""
+    return redirect("/question/0")
+
+
+
+@app.route('/question/<int:ques_id>')
+def questions(ques_id):
+    """"displays questions"""
+
+    if(len(responses) == len(survey.questions)):
+        # if its the end of the survey display thank you
+        return redirect("/thanks")
+    if(len(responses) != ques_id):
+        #if user is trying to access a question not in the survey
+        flash(f"Invalid question request")
+        return redirect(f"/question/{len(responses)}")
+    if(responses is None):
+        #if user tries to move on without answering
+        return redirect("/")
+
+    question = survey.questions[ques_id]
+    return render_template('questions.html',question_num=ques_id, question=question)
+
+
+@app.route('/answer', methods=["POST"])
+def get_answers():
+    """gets users answer"""
+    answer = request.form['answer']
+    responses.append(answer)
+
+    if (len(responses) == len(survey.questions)):
+        #if the users has answered the last question takes them to the complete page
+        return redirect("/thanks")
+    else:
+        return redirect(f"/question/{len(responses)}")
+
+
+@app.route('/thanks')
+def survey_complete():
+    """displays survey complete page"""
+    return render_template("complete.html")
